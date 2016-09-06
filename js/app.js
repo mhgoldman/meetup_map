797330d025c4bbc3849deb817e984d025b667498
Martin Goldman <mgoldman@skytap.com>
2016-09-06 10:52:57 -0400

    zoom

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 var markersArray = [];
+var map = null;
 
 function initMap() {
-  var map = new google.maps.Map($('.map').get()[0], {
+  map = new google.maps.Map($('.map').get()[0], {
     zoom: 12,
     center: {lat: 40.730610, lng: -73.935242}
   });
@@ -11,18 +12,19 @@ function initMap() {
     startSpinning();
     clearMarkers();
     e.preventDefault();
-    geocodeAddress(geocoder, map);
+    geocodeAddress(geocoder);
+    $('.address').blur();
   });
 }
 
-function geocodeAddress(geocoder, map) {
+function geocodeAddress(geocoder) {
   var address = $('.address').val();
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === 'OK') {
       loc = results[0].geometry.location;
       map.setCenter(loc);
-      addMarker(map, loc, "http://maps.google.com/mapfiles/ms/icons/green-dot.png", address)
-      findMeetups(map, loc);
+      addMarker(loc, "http://maps.google.com/mapfiles/ms/icons/green-dot.png", address, google.maps.Marker.MAX_ZINDEX + 1)
+      findMeetups(loc);
     } else {
       alert('Location not found: ' + status);
       stopSpinning();
@@ -30,18 +32,19 @@ function geocodeAddress(geocoder, map) {
   });
 }
 
-function findMeetups(map, loc) {
+function findMeetups(loc) {
   lat = loc.lat();
   lng = loc.lng();
 
   $.getJSON('https://api.meetup.com/2/groups?&key=165f2e85d23027f478a535b3436&photo-host=public&lon=' + lng + '&lat=' + lat + '&callback=?', function(data) {
-    numResults = Math.min(data.results.length, 25);
+    numResults = data.results.length;
 
     for (i=0; i<numResults; i++) {
       res = data.results[i];
-      addMarker(map, {lat: res.lat, lng: res.lon}, null, res.name);
+      addMarker({lat: res.lat, lng: res.lon}, null, res.name);
     }
 
+    updateBounds();
     stopSpinning();
   });
 
@@ -61,17 +64,29 @@ function clearMarkers() {
   for (i=0; i<markersArray.length; i++) {
     markersArray[i].setMap(null);
   }
+
+  markersArray = [];
 }
 
-function addMarker(map, pos, icon, title) {
+function addMarker(pos, icon, title, zindex) {
   var marker = new google.maps.Marker({
     map: map,
     position: pos,
     icon: icon,
-    title: title
+    title: title,
+    zIndex: zindex
   });
 
   markersArray.push(marker);
+}
+
+function updateBounds() {
+  var bounds = new google.maps.LatLngBounds();
+  for (var i = 0; i < markersArray.length; i++) {
+   bounds.extend(markersArray[i].getPosition());
+  }
+
+  map.fitBounds(bounds);
 }
 
 function resizeMap() {
